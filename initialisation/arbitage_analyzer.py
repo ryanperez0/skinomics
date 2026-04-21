b271575e516633510d023defe364fd4d4ab8b7d7
ryanperez0 <ryanperez3005@gmail.com>
2026-04-21 12:37:29 +0300

    Refactor comments and enhance break-even price output
    Updated comments to clarify the objective and constraints of the script. Removed unfinished code sections and added print statements for break-even prices.

--- initialisation/arbitage_analyzer.py
+++ initialisation/arbitage_analyzer.py
@@ -1,5 +1,8 @@
-# Lets take 3 of the most popular assets on the market
-# Note: every asset has a 'class' - the skin may be the same but the lower the 'float' the more expensive
+# Objective: To identify price discrepancies between Steam and 3rd party markets
+# Constraint: Incorporating a 15% Steam "sink" and 3rd party withdrawal frictions.
+
+# Note: This file is unfinished: code framework will be used later once pandas is integrated - moving onto CSV manipulation
+
 
 # Field-Tested is the median asset class
 steam_prices = {
@@ -8,7 +11,7 @@ steam_prices = {
 }
 
 float_prices = {
-"AK-47 | Inheritance (Field-Tested" : 48.99 , "M4A1-S | Printstream (Field-Tested)" : 213.19 ,
+"AK-47 | Inheritance (Field-Tested)" : 48.99 , "M4A1-S | Printstream (Field-Tested)" : 213.19 ,
     "USP-S | Jawbreaker (Field-Tested)" : 4.32
 }
 
@@ -28,13 +31,20 @@ for skin_name, skin_price in steam_prices.items():
     breakevensteam = skin_price / (1 - steam_fee)
     f_price = float_prices[skin_name]
 
-    steaminput = input(print(f"Would you like the break even price for {skin_name} on Steam? (Y/N)"))
-    if steaminput == "Y" or steaminput == "y":
-        print(f" The final break-even steam price for {skin_name} is {round(breakevensteam, 2)}, adjusted to 1% risk fee would be {round(breakevensteam * (1 + slippage), 2)}")
-
 print()
 
 for skin_name, skin_price in float_prices.items():
     breakevenfloat = skin_price / (1 - csfloat_withdraw) * (1 - csfloat_deposit)
     print(f" The final break-even CSFloat price for {skin_name} is {round(breakevenfloat, 2)}")
+
+
+
+print(f"""
+Asset: {skin_name}
+--------------------------
+Steam Break-even: {breakevensteam}
+Float Break-even: {breakevenfloat}
+Gross Spread:
+Decision: [Arbitrage Viable / Avoid]
+""")
 
